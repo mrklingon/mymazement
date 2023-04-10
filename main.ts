791a7bae19a58b1706189e8090d3755f3794b441
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2023-04-10 14:42:51 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -25,39 +25,16 @@ function createWorld (rad: number) {
 function West (Spot: number) {
     return (Usize + (Spot - 1)) % Usize
 }
-function Test () {
-    world[Loc] = 0
-    R = randint(0, 3)
-    if (R == 0) {
-        Loc = North(Loc)
-    }
-    if (R == 1) {
-        Loc = East(Loc)
-    }
-    if (R == 2) {
-        Loc = South(Loc)
-    }
-    if (R == 3) {
-        Loc = West(Loc)
-    }
-    dispWorld()
-}
 function North (Spot: number) {
     return (Usize + (Spot - radius)) % Usize
 }
 function Xval (spot: number) {
     return spot % radius
 }
-let R = 0
 let world: number[] = []
-let Loc = 0
 let Usize = 0
 let radius = 0
 radius = 5
 Usize = radius * radius
-Loc = 0
 createWorld(radius)
 dispWorld()
-for (let index = 0; index < 15; index++) {
-    Test()
-}
