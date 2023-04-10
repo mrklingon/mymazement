b2b475e2289b1e5b0fb51dd53f2020f1e35b8d41
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2023-04-10 14:28:56 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,17 +10,31 @@ function dispWorld () {
         }
     }
 }
+function East (Spot: number) {
+    return (Usize + (Spot + 1)) % Usize
+}
+function South (Spot: number) {
+    return (Usize + (Spot + radius)) % Usize
+}
 function createWorld (rad: number) {
     world = []
     for (let index = 0; index < rad * rad; index++) {
         world.push(1)
     }
 }
+function West (Spot: number) {
+    return (Usize + (Spot - 1)) % Usize
+}
+function North (Spot: number) {
+    return (Usize + (Spot - radius)) % Usize
+}
 function Xval (spot: number) {
     return spot % radius
 }
 let world: number[] = []
+let Usize = 0
 let radius = 0
 radius = 5
+Usize = radius * radius
 createWorld(radius)
 dispWorld()
