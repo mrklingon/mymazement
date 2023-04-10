3771a7fdf083178a8b5ddf7ce3ade47d5d5b96cf
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2023-04-10 16:46:47 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,28 @@
 function Yval (spot: number) {
     return Math.trunc(spot / radius)
 }
+function bldMaze () {
+    world[loc] = 0
+    dispWorld()
+    while (loc != goal) {
+        dir = randint(0, 3)
+        if (dir == 0) {
+            loc = North(loc)
+        }
+        if (dir == 1) {
+            loc = East(loc)
+        }
+        if (dir == 2) {
+            loc = South(loc)
+        }
+        if (dir == 3) {
+            loc = West(loc)
+        }
+        world[loc] = 0
+        basic.pause(100)
+        dispWorld()
+    }
+}
 function dispWorld () {
     for (let index = 0; index <= radius * radius - 0; index++) {
         if (world[index] == 1) {
@@ -10,6 +32,14 @@ function dispWorld () {
         }
     }
 }
+input.onButtonPressed(Button.A, function () {
+    start = 0
+    loc = 0
+    goal = Usize - 1
+    createWorld(radius)
+    dispWorld()
+    bldMaze()
+})
 function East (Spot: number) {
     return (Usize + (Spot + 1)) % Usize
 }
@@ -31,10 +61,18 @@ function North (Spot: number) {
 function Xval (spot: number) {
     return spot % radius
 }
+let dir = 0
 let world: number[] = []
+let goal = 0
+let loc = 0
+let start = 0
 let Usize = 0
 let radius = 0
+let maze = false
 radius = 5
 Usize = radius * radius
+start = 0
+loc = 0
+goal = Usize - 1
 createWorld(radius)
 dispWorld()
